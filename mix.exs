3e3124dd082b9422b7f9e6fdfac1bd44ac043e41
Aaron Renner <aaron@renner.live>
2019-07-27 08:32:11 -0600

    Add ExDoc for visualizing APIs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ZoneMealTrackerUmbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: dialyzer()
+      dialyzer: dialyzer(),
+      docs: docs()
     ]
   end
 
@@ -19,7 +20,8 @@ defmodule ZoneMealTrackerUmbrella.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
@@ -27,6 +29,12 @@ defmodule ZoneMealTrackerUmbrella.MixProject do
     [
       ignore_warnings: ".dialyzer_ignore.exs",
       list_unused_filters: true
+    ]
+  end
+
+  defp docs do
+    [
+      ignore_apps: [:zone_meal_tracker_web]
     ]
   end
 end
